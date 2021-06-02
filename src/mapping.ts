bacb5f373904db83a34ade62695902b397866040
hamza ahmed <hamza.ahmed742@gmail.com>
2021-06-02 21:43:38 +0500

    inital commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,45 +1,15 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import { Brastilava, OwnerSet } from "../generated/Brastilava/Brastilava"
-import { ExampleEntity } from "../generated/schema"
+import { OwnerSet } from "../generated/Brastilava/Brastilava"
+import { Brastilava } from "../generated/schema"
 
 export function handleOwnerSet(event: OwnerSet): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
+  let entity = Brastilava.load(event.transaction.from.toHex())
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
+    entity = new Brastilava(event.transaction.from.toHex())
     entity.count = BigInt.fromI32(0)
   }
-
-  // BigInt and BigDecimal math are supported
   entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
   entity.oldOwner = event.params.oldOwner
   entity.newOwner = event.params.newOwner
-
-  // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
 }
